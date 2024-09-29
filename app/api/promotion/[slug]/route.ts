a4c8782ebe456a946cd1247829a3e7e27e5aa218
satria bahari <satriaabaharii@gmail.com>
2024-09-29 18:47:41 +0700

    refactor: add some API

---
+++ app/api/promotion/[slug]/route.ts
@@ -0,0 +1,55 @@
+import { createClient } from "@/common/utils/server";
+import { NextResponse } from "next/server";
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { slug: string } },
+) => {
+  const supabase = createClient();
+  try {
+    const { data } = await supabase
+      .from("promotions")
+      .select()
+      .eq("id", params.slug)
+      .single();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { slug: string } },
+) => {
+  const supabase = createClient();
+  try {
+    const requestData = await req.json();
+    await supabase.from("promotions").update(requestData).eq("id", params.slug);
+    return NextResponse.json("Data Updated Successfully", { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
+
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { slug: string } },
+) => {
+  const supabase = createClient();
+  try {
+    await supabase.from("promotions").delete().eq("id", params.slug).single();
+    return NextResponse.json("Data Deleted Successfully", { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
